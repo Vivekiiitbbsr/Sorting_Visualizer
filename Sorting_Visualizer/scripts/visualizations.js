1f972cdbe2fcd2baa66e47c1926b4121da6c0b9b
Vivek Mishra <97890608+Vivekiiitbbsr@users.noreply.github.com>
2023-12-29 13:25:06 +0530

    Update visualizations.js

--- Sorting_Visualizer/scripts/visualizations.js
+++ Sorting_Visualizer/scripts/visualizations.js
@@ -1,6 +1,6 @@
 /*
     *****************
-    DONE BY:-   TUMMALA KETHAN
+    DONE BY:-   Vivek Mishra
     
     *****************
 */
@@ -59,7 +59,7 @@ function enable_buttons()
 
 /*
     *****************
-    DONE BY:-   TUMMALA KETHAN
+    DONE BY:-   Vivek Mishra
     
     *****************
 */
