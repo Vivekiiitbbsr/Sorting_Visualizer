c5b918fe3d24fad263a29d8b69c811fca74bf6c8
Vivek Mishra <97890608+Vivekiiitbbsr@users.noreply.github.com>
2023-12-29 13:23:18 +0530

    Update main.js

--- Sorting_Visualizer/scripts/main.js
+++ Sorting_Visualizer/scripts/main.js
@@ -1,6 +1,6 @@
 /*
     *****************
-    DONE BY:-   TUMMALA KETHAN
+    DONE BY:-   Vivek Mishra
     
     *****************
 */
@@ -94,7 +94,7 @@ function runalgo()
 
 /*
     *****************
-    DONE BY:-   TUMMALA KETHAN
+    DONE BY:-  Vivek Mishra
     
     *****************
 */
